ed95b97b97053c50dccd7c268cb2a11c21591491
CarlosDubon <carlosminerodubon@gmail.com>
2017-07-06 11:32:40 -0600

    G

---
+++ src/GUI/Login.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import javax.swing.JFrame;
+import javax.swing.*;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Login extends JFrame{
+    public Login(int w, int h, String title){
+        super(title);
+        JTextField txtCod = new JTextField(17);
+        JPasswordField txtPass = new JPasswordField(17);
+        JButton btnValidar = new JButton("Validar");
+        JPanel jpDatos = new JPanel();
+        GridLayout gl = new GridLayout(5,0,1,10);
+        
+        
+        jpDatos.setLayout(gl);
+        jpDatos.add(new JLabel("Codigo"));
+        jpDatos.add(txtCod);
+        jpDatos.add(new JLabel("Password"));
+        jpDatos.add(txtPass);
+        jpDatos.add(btnValidar);
+        //jpDatos.add(btnValidar);
+        
+        JPanel jpBotones = new JPanel();
+        GridLayout gl2 = new GridLayout(4,0,3,10);
+        JButton btnSolAcceso = new JButton("Solicitar Acceso");
+        JButton btnAbrirP = new JButton("Abrir Puerta");
+        JButton btnCancel = new JButton("Cancel");
+        
+        jpBotones.setLayout(gl2);
+        jpBotones.add(new JLabel(""));
+        jpBotones.add(btnSolAcceso);
+        jpBotones.add(btnAbrirP);
+        jpBotones.add(btnCancel);
+        
+        
+
+        
+        Container cp = getContentPane();
+        FlowLayout layout = new FlowLayout();        
+        cp.setLayout(layout);
+        cp.add(jpDatos);
+        cp.add(jpBotones);
+        
+        
+        setSize(w,h);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setFocusable(true);
+        setLocationRelativeTo(null);
+                
+                
+        setResizable(false);
+    }
+    
+}
--- src/programaa/ProgramaA.java
+++ src/programaa/ProgramaA.java
@@ -5,6 +5,8 @@
  */
 package programaa;
 
+import GUI.Login;
+
 /**
  *
  * @author Carlos
@@ -16,6 +18,7 @@ public class ProgramaA {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Login login = new Login(250,400,"INICIAR SESION");
     }
     
 }
